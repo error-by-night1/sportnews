e167a0cc2e8ba04954d7835467ad60cf64fe8623
zied7 <zied.sradkii@gmail.com>
2020-12-22 20:28:14 +0100

    data base

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./database/db");
+//---------------------------------------------------
+connectDB();
+//---------------------------------------------------
+
 const port = process.env.PORT || 8080;
 let cors = require("cors");
 //---------------------------------------------------
@@ -11,5 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("<h>start working team</h1>");
 });
+//---------------------------------------------------
+// Define routes
+app.use("/Router/article", require("./Router/article"));
+app.use("/Router/news", require("./Router/news"));
+
 //------------------------------------------------------
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
